eb076d2011922d56ac2f6112f1e4dda208bbb8d1
Narayanswamy <swamy2492@gmail.com>
2018-06-02 01:07:29 +0530

    Fixed bugs with Ingredients and Directions, Added User profile with username

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,8 @@
 class Recipe < ActiveRecord::Base
-
+	belongs_to :user
 	has_many :ingredients, dependent: :destroy
 	has_many :directions, dependent: :destroy
+
 
 	accepts_nested_attributes_for :ingredients,
   															reject_if: proc { |attributes| attributes['name'].blank? },
